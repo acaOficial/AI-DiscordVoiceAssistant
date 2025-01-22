f6b80f334e037cee344e56222e619455666b3566
acaymo <acaimopcgc@gmail.com>
2025-01-22 22:11:26 +0000

    fix: Fixed decoder function

--- discord/commands/voice.ts
+++ discord/commands/voice.ts
@@ -29,59 +29,6 @@ async function execute(connection: VoiceConnection): Promise<void> {
     });
 }
 
-/**
- * Función para crear un oyente de audio.
- * @param receiver - El receptor de voz de tipo `VoiceReceiver`.
- * @param userId - El ID del usuario de tipo `string`.
- * @param buffer - La instancia de `VoiceBuffer` para almacenar los datos de voz.
- */
-// async function createListener(receiver: VoiceReceiver, userId: string, buffer: VoiceBuffer): Promise<void> {
-//     //const encoder = new prism.opus.Encoder({ frameSize: 960, channels: 2, rate: 48000 });
-//     //const encoder = new OpusEncoder({sampleRate: 48000, channels: 2});
-
-//     const SAMPLE_RATE = 8000; // Tasa de muestreo en Hz
-//     const CHANNELS = 2; // Número de canales: 1 para mono, 2 para estéreo
-//     const APPLICATION = OpusEncoder.Application.AUDIO; // Uso genérico de audio
-
-//     // Crear una instancia del codificador OpusEncoder
-    
-
-//     const opusStream = receiver.subscribe(userId, {
-//         end: {
-//             behavior: EndBehaviorType.AfterSilence,
-//             duration: 3000,
-//         },
-//     });
-
-//     opusStream.on('data', (chunk) => {
-//         try {
-//             const encoder = new OpusEncoder(SAMPLE_RATE, CHANNELS, APPLICATION);
-//             // Codificar el chunk de audio
-//             const encoded = encoder.encode(chunk); // Asumiendo que chunk es un Buffer
-//             if (!encoded || encoded.length === 0) {
-//                 console.log('No se pudieron codificar muestras.');
-//                 return;
-//             }
-            
-
-//             // Añadir el chunk de audio codificado al buffer
-//             buffer.addChunk(Buffer.from(encoded));
-//             encoder.delete();
-//         } catch (error) {
-//             console.error('Error al decodificar el paquete Opus:', error);
-//         }
-//     });
-
-//     opusStream.on('end', async () => {
-//         console.log('El flujo de audio ha terminado.');
-//         const text = await buffer.convertToText();
-//         console.log(`Texto resultante: ${text}`);
-//     });
-
-//     opusStream.on('error', (err) => {
-//         console.error('Error en la transmisión de audio:', err);
-//     });
-// }
 
 /**
  * Función para crear un oyente de audio.
@@ -92,8 +39,6 @@ async function execute(connection: VoiceConnection): Promise<void> {
 async function createListener(receiver: VoiceReceiver, userId: string, buffer: VoiceBuffer): Promise<void> {
     // Crear una instancia del codificador OpusEncoder
     const encoder = new OpusEncoder(48000, 2);
-
-
 
     const opusStream = receiver.subscribe(userId, {
         end: {
@@ -106,20 +51,22 @@ async function createListener(receiver: VoiceReceiver, userId: string, buffer: V
         try {
 
             // Codificar el chunk de audio
-            const encoded = encoder.encode(chunk); // Asumiendo que chunk es un Buffer
-            if (!encoded || encoded.length === 0) {
+            const decoded = encoder.decode(chunk); // Asumiendo que chunk es un Buffer
+            if (!decoded || decoded.length === 0) {
                 console.log('No se pudieron codificar muestras.');
                 return;
             }
             
             // Añadir el chunk de audio codificado al buffer
-            buffer.addChunk(Buffer.from(encoded));
+            buffer.addChunk(Buffer.from(decoded));
         } catch (error) {
             console.error('Error al decodificar el paquete Opus:', error);
         }
     });
 
     opusStream.on('end', async () => {
+
+        // Añadir el buffer acumulado al buffer de voz
         console.log('El flujo de audio ha terminado.');
         const text = await buffer.convertToText();
         console.log(`Texto resultante: ${text}`);
--- discord/dependencies.ts
+++ discord/dependencies.ts
@@ -1,4 +1,4 @@
 import { GoogleParser } from "./utils/parser/google/googleParser";
 import { AiParser } from "./utils/parser/ai/aiParser";
 
-export const voiceParser = new AiParser('localhost', 5067);
+export const voiceParser = new AiParser('127.0.0.1', 5067);
